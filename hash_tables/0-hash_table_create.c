d976e60f17bf23df933dbd2e869a78ec66e9a499
AhmadNawabi <ahmadshamoon0505@gmail.com>
2023-11-17 19:48:12 -0600

    Change hash table creat file

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,21 +1,32 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - Create a Hash Table
- * @size: The size of the hash  table
- * Descreption : Alloctaing a Hash Table using Given Size
- * Return: NULL Error Occurred Or A Hash Table
+ * hash_table_create - This function will create a hash table
+ * @size: size for the table/array to be.
+ *
+ * Return: a pointer to the table
  */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table;
+	hash_table_t *table;
+	unsigned long int idx = 0;
 
-	hash_table = malloc(sizeof(hash_table_t));
-	if (hash_table == NULL)
+	if (size <= 0)
 		return (NULL);
-	hash_table->size = size;
-	hash_table->array = malloc(sizeof(hash_table->array) * size);
-	if (hash_table->array == NULL)
+
+	table = malloc(sizeof(hash_table_t));
+	if (!table)
 		return (NULL);
-	return (hash_table);
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+
+	while (idx < size)
+		table->array[idx++] = NULL;
+	return (table);
 }

be9962ad1be5f8c66ea499bcf7811a7c5166a50a
AhmadNawabi <ahmadshamoon0505@gmail.com>
2023-11-17 19:06:29 -0600

    hash_table_file

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -6,8 +6,9 @@
  * Descreption : Alloctaing a Hash Table using Given Size
  * Return: NULL Error Occurred Or A Hash Table
  */
-hash_table_t *hash_table_create(unsigned long int size){
-	hash_table_t *hash_table;
+hash_table_t * hash_table_create(unsigned long int size)
+{
+	hash_table_t * hash_table;
 
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
